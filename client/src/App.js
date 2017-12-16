72c7fcdb995c2c2909e4ba70a289caebc54d0501
Connie (Moon Hyung) Choi <connie_choi89@hotmail.com>
2017-12-16 02:41:55 +0000

    make placeholder for user info - profile page

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,14 @@ function Profile() {
     <div>
       <h1>Profile</h1>
       <p>Update Your Profile</p>
-      <p>Here will be one (or several?) form(s) to update profile details</p>
+      <form action="/progress" method="post">
+        Email <input type="text" name="email" placeholder="Email"/><br/>
+        First name<input type="text" name="fname" placeholder="First name"/><br/>
+        Last name<input type="text" name="lname" placeholder="Last name"/><br/>
+        Location<input type="text" name="location" placeholder="Location"/><br/>
+        Why are you learning to code?<input type="text" name="motivation" placeholder=""/><br/>
+        <input type="submit" value="Submit"/>
+      </form>
     </div>
   );
 }
