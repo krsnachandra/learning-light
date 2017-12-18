199bf368d58d832cc7fe28fae76709ece3af8aed
Connie (Moon Hyung) Choi <connie_choi89@hotmail.com>
2017-12-18 19:28:17 +0000

    move profile component to Profile.jsx

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,13 @@
 import React, { Component } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import logo from './logo.svg';
+import Profile from './Profile.jsx';
 
 function Progress() {
   return <h1>Progress</h1>;
 }
 function Home() {
   return <Redirect to="/progress" />;
-}
-function Profile() {
-  return (
-    <div>
-      <h1>Profile</h1>
-      <p>Update Your Profile</p>
-      <form action="/progress" method="post">
-        Email <input type="text" name="email" placeholder="Email"/><br/>
-        First name<input type="text" name="fname" placeholder="First name"/><br/>
-        Last name<input type="text" name="lname" placeholder="Last name"/><br/>
-        Location<input type="text" name="location" placeholder="Location"/><br/>
-        Why are you learning to code?<input type="text" name="motivation" placeholder=""/><br/>
-        <input type="submit" value="Submit"/>
-      </form>
-    </div>
-  );
 }
 
 class App extends Component {
