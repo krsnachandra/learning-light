a58196eb3a53b45e08bc9b0e1a24ef599f66a38d
Krsna Chandra Musterer <chandra@Krsnas-MacBook-Air.local>
2017-12-18 18:23:27 -0800

    mocked up the home page

--- client/src/App/Home.jsx
+++ client/src/App/Home.jsx
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect, Link } from 'react-router-dom';
 
 function Home() {
     return <div>
-      <img src="/htmlcss.png" alt="htmlcss"/>;
-    </div>
+      <br/>
+      <br/>
+      <br/>
+      <h2 align="center">Learn the basics of web and iOS development <br/>
+        with the team from Lighthouse Labs.</h2>
+      <br/>
+      <br/>
+      <br/>
+      <div align="center">
+        <img src="/htmlcss.png" alt="htmlcss" height="400" width="240"/>
+        <Link to='/course'>
+          <img src="/js1.png" alt="js1" height="400" width="240"/>
+        </Link>
+        <img src="/js2.png" alt="js2" height="400" width="240"/>
+        <img src="/ios.png" alt="ios" height="400" width="240"/>
+      </div>
+    </div>;
   }
 
   export default Home;
