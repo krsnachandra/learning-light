c258df351ef6c4c2f8789d26978fd3c687d56872
Connie (Moon Hyung) Choi <connie_choi89@hotmail.com>
2017-12-19 17:43:30 -0800

    using bootstrap for register page

--- client/src/App/Register.jsx
+++ client/src/App/Register.jsx
@@ -3,15 +3,70 @@ import React, { Component } from 'react';
 function Register() {
   return <div>
     <h1>Register</h1>
-      <form action="/register" method="post">
-          First name<input type="text" name="fname" placeholder="First name" /><br />
-          Last name<input type="text" name="lname" placeholder="Last name" /><br />
-          Location<input type="text" name="location" placeholder="Location" /><br />
-          Why are you learning to code?<input type="text" name="motivation" placeholder="" /><br />
-          Email <input type="text" name="email" placeholder="Email" /><br />
-          Password<input type="text" name="password" placeholder="Password" /><br />
-        <input type="submit" value="Submit" />
-      </form>
+    <fieldset>
+    <div class="form-group">
+      <label class="col-md-4 control-label" for="textinput">First Name</label>  
+      <div class="col-md-4">
+      <input id="textinput" name="textinput" placeholder="Insert your First Name" class="form-control input-md" required="" type="text" />
+      <span class="help-block"> </span>  
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label class="col-md-4 control-label" for="textinput">Last Name</label>  
+      <div class="col-md-4">
+      <input id="textinput" name="textinput" placeholder="Insert your Last Name" class="form-control input-md" required="" type="text" />
+      <span class="help-block"> </span>  
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label class="col-md-4 control-label" for="textinput">Location</label>  
+      <div class="col-md-4">
+      <input id="textinput" name="textinput" placeholder="Insert your Location" class="form-control input-md" required="" type="text" />
+      <span class="help-block"> </span>  
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label class="col-md-4 control-label" for="textinput">Motivation</label>  
+      <div class="col-md-4">
+      <input id="textinput" name="textinput" placeholder="Why are you learning to code?" class="form-control input-md" required="" type="text" />
+      <span class="help-block"> </span>  
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label class="col-md-4 control-label" for="textinput">Email</label>  
+      <div class="col-md-4">
+      <input id="textinput" name="textinput" placeholder="Insert your Email" class="form-control input-md" required="" type="text" />
+      <span class="help-block"> </span>  
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label class="col-md-4 control-label" for="textinput">Password</label>  
+      <div class="col-md-4">
+      <input id="textinput" name="textinput" placeholder="Insert your Password" class="form-control input-md" required="" type="text" />
+      <span class="help-block"> </span>  
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label class="col-md-4 control-label" for="textinput">Confirm Password</label>  
+      <div class="col-md-4">
+      <input id="textinput" name="textinput" placeholder="Confirm your Password" class="form-control input-md" required="" type="text" />
+      <span class="help-block"> </span>  
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label class="col-md-4 control-label" for="singlebutton"> </label>
+      <div class="col-md-4">
+        <button id="singlebutton" name="singlebutton" class="btn btn-primary">Submit</button>
+      </div>
+    </div>
+    </fieldset>
     </div>;
 }
 
