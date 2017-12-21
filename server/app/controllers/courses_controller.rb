86edb410955c868a99af72baeb916f5c18da8e48
Connie (Moon Hyung) Choi <connie_choi89@hotmail.com>
2017-12-21 12:16:36 -0800

    courses with instructors' info on /courses API route

--- server/app/controllers/courses_controller.rb
+++ server/app/controllers/courses_controller.rb
@@ -2,8 +2,7 @@ class CoursesController < ApplicationController
 
     def index
         @courses = Course.all.order(id: :asc)
-       
-        render json: @courses
+        render json: @courses.to_json(include: :instructor)
     end
 
 end
