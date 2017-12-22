e5defd1791be7ba54a4062aee335612815635105
Krsna Chandra Musterer <chandra@Krsnas-MacBook-Air.local>
2017-12-21 18:54:59 -0800

    playing with axios trying to pass data

---
+++ client/src/App/Smrt.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Smrt extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      section: ''
+    }
+
+  }
+
+  componentDidMount() {
+    axios.get('/courses/3/sections/3', {
+      // params: {
+      //   ID: 3
+      // }
+    })
+    .then(function (response) {
+      console.log(response);
+      console.log(response.data.content);
+      // console.log(response);
+      // const content = response.content;
+      // this.setState({section: content});
+
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+    // this.setState({section});
+
+  }
+
+  render() {
+    
+    return (
+        <div>
+          <h1>Hello to Smart React Component</h1>
+        </div>
+
+    );
+  }
+}
+
+export default Smrt;
--- client/src/App/index.jsx
+++ client/src/App/index.jsx
@@ -10,27 +10,67 @@ import Register from './Register';
 import Review from './Review';
 // import Footer from './Footer';
 import CourseCompleted from './CourseCompleted';
+import Smrt from './Smrt';
+import axios from 'axios';
 
 
 class App extends Component {
 
+  constructor(props){
+    super(props);
+    this.state = {
+      test : 'Hello this is a great content',
+      content: ''
+    }
+  }
+
+  componentWillMount() {
+    axios.get('/courses/3/sections/3', {
+      // params: {
+      //   ID: 3
+      // }
+    })
+    .then(function (response) {
+      this.setState({
+        content: response.data.content
+      })
+      //console.log(response.data.content);
+      // console.log(response);
+      // const content = response.content;
+      // this.setState({section: content});
+
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+    // this.setState({section});
+
+  }
+
+
   render() {
     return (
       <div>
-        <Navbar />
-        <div className="App container">
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/profile" exact component={Profile} />
-            <Route path="/progress" exact component={Progress} />
-            <Route path="/login" component={Login} />
-            <Route path="/register" component={Register} />
-            <Route path="/review" component={Review} />
-            <Route path="/comp" component={CourseCompleted} />
-            <Route path="/:coursename" component={Course} />
-          </Switch>
-        </div>
-        {/* {<Footer />} */}
+        {/* <Smrt> */}
+          <Navbar />
+          <div className="App container">
+            <Switch>
+              {/* <Route path="/" exact component={()=><Home test={this.state.test}/>} /> */}
+              <Route exact path='/' render={(props) => (
+                <Home test={this.state.test} />
+              )}/>
+
+              <Route path="/profile" exact component={Profile} />
+              <Route path="/progress" exact component={Progress} />
+              <Route path="/login" component={Login} />
+              <Route path="/register" component={Register} />
+              <Route path="/review" component={Review} />
+              <Route path="/comp" component={CourseCompleted} />
+              <Route path="/:coursename" component={Course} />
+            </Switch>
+          </div>
+          {/* {<Footer />} */}
+        {/* </Smrt> */}
       </div>);
   }
 }
