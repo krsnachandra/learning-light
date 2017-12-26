7455e06195137dfd4ed69859d78701c696ed6a5b
Krsna Chandra Musterer <chandra@Krsnas-MacBook-Air.local>
2017-12-26 13:32:55 -0800

    changed API routes for easier access on front end

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -3,10 +3,7 @@
 
 
 Rails.application.routes.draw do
-  
-  # resources :courses, only: [:index] do
-  #   resources :sections, only: [:show]
-  # end
+
   get 'courses', to: 'courses#index', as: :courses
   get ':coursename', to: 'courses#show', as: :coursename
   get ':coursename/:sectionname', to: 'sections#show', as: :sectionname
