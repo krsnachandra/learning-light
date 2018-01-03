dd6687000c83e90c1d49b131f1af2e7f5181ad44
krsnachandra <chandra@Krsnas-MacBook-Air.local>
2018-01-03 14:44:33 -0800

    using real data for course summary

--- client/src/App/CourseSummary.jsx
+++ client/src/App/CourseSummary.jsx
@@ -1,49 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PublishedReviews from './PublishedReviews';
+import {getCourse} from '../course-service';
 
-export default function CourseSummary () {
-  return (
-    <div>
-    <h2>
-        Related courses:
-    </h2>
-    <div>
-      Javascript Essentials Part II
-    </div>
+function Loading() {
+  return <h1>Loading...</h1>;
+}
 
-    <h2>
-      Course Details
-    </h2>
-    <div>
-      <p>
-        In this free course, you'll be learning how to build your own web-based chat app similar to Twitch or Slack using JavaScript and other modern tools like Node.js and Web Sockets. The goal of this course is to introduce you to the fundamentals of building web-based applications using some of the most prominent and widely used programming technologies! </p><p>If you're new to programming, we recommend you start with our <a href="http://lighthouse-labs.thinkific.com/courses/html-and-css" target="_blank" rel="noopener noreferrer">HTML & CSS Essentials</a> course.
-        </p>
-    </div>
-    <div>
-      <h2>
-        Instructor
-      </h2>
-    </div>
-    <div>
-      <img src='https://s3.amazonaws.com/thinkific/instructors/000/046/9561488219777.small.png?1488219777' alt='David VanDusen' className='instructor__img' />
-    </div>
-    <div>
-      David VanDusen
-    </div>
-    <div>
-      Web Developer & Instructor
-    </div>
-    <div>
-      <p>David is a sort of web dev Don Quixote, helping out wherever he is needed. He has grappled with his share of technological windmills yet continues his chivalrous fight for clean, beautiful code. That said, David is a renaissance man and pursues fine arts and music in his leisure.</p>
-    </div>
-    <div>
-      <h3>
-        Reviews (1)
-      </h3>
-      <h4>
-        5 Stars! Thee Beste Course That Ever Coursed
-      </h4>
-        by Geoffrey Chaucer
-      </div>
-    </div>);
+class CourseSummary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true
+    };
   }
+
+  componentDidMount(){
+    getCourse(this.props.coursename)
+    .then((course) => {
+      this.setState({loading: undefined, ...course});
+    });
+  }
+  
+  render() {
+    console.log(this.props);
+    if (this.state.loading) {
+      return (<Loading />);
+    }
+    return (
+      <div>
+        <h2>Course Details</h2>
+      <div>
+        <p>{this.state.description}</p>
+      </div>
+      <div>
+        <h2>Instructor</h2>
+      </div>
+      <div>
+        <img src='https://s3.amazonaws.com/thinkific/instructors/000/046/9561488219777.small.png?1488219777' alt='David VanDusen' className='instructor__img' />
+      </div>
+      <div>
+        David VanDusen
+      </div>
+      <div>
+        Web Developer & Instructor
+      </div>
+      <div>
+        <p>David is a sort of web dev Don Quixote, helping out wherever he is needed. He has grappled with his share of technological windmills yet continues his chivalrous fight for clean, beautiful code. That said, David is a renaissance man and pursues fine arts and music in his leisure.</p>
+      </div>
+      <div>
+        <h3>
+          Reviews (1)
+        </h3>
+        <h4>
+          5 Stars! Thee Beste Course That Ever Coursed
+        </h4>
+          by Geoffrey Chaucer
+        </div>
+      </div>);
+    }
+  }
+
+  export default CourseSummary;
