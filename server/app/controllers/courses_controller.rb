8901af8bb16b2bca93faaae1c3557b511e32f27e
krsnachandra <chandra@Krsnas-MacBook-Air.local>
2018-01-03 15:47:09 -0800

    added reviews to course show route

--- server/app/controllers/courses_controller.rb
+++ server/app/controllers/courses_controller.rb
@@ -7,7 +7,7 @@ class CoursesController < ApplicationController
 
     def show
         @course = Course.find_by_coursename params[:coursename]
-        render json: @course.to_json(include: [:instructor, {chapters: {include: :sections}}])
+        render json: @course.to_json(include: [:instructor, :reviews, {chapters: {include: :sections}}])
     end
 
 end
