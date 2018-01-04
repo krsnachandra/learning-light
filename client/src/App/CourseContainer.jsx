752ec7c9986c48ef0d2078931194abb55191ddf0
V. Anastassiou <v.anastassiou@gmail.com>
2018-01-03 22:03:01 -0800

    New images added

--- client/src/App/CourseContainer.jsx
+++ client/src/App/CourseContainer.jsx
@@ -2,16 +2,6 @@ import React, { Component } from 'react';
 import {getCourse, Loading} from '../course-service';
 import CourseContent from './CourseContent';
 
-// TODO: make display actually dependent on props
-// Based on course ID (from Link to React Router declaration in Home), display:
-// CourseSummary if course not started
-// CourseContent if course started
-// CourseCompleted if course done
-
-function Loading() {
-  return <h1>Loading...</h1>;
-}
-
 class CourseContainer extends Component {
   constructor(props) {
     super(props);
