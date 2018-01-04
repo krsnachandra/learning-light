8f2d071d11f7463eeee4a712efd480b248e2f3c5
Connie (Moon Hyung) Choi <connie_choi89@hotmail.com>
2018-01-04 14:55:58 -0800

    fixed renamed badge

--- client/src/App/CourseCompleted.jsx
+++ client/src/App/CourseCompleted.jsx
@@ -7,7 +7,7 @@ function CourseCompleted () {
   return <div align="center">
     <h1>Congratulations!</h1>
     <h2>You have completed the JavaScript Essentials I course!</h2>
-    <img src="/jsbadge.png" alt="jsbadge"/><br/>
+    <img src="/badge-js.png" alt="jsbadge"/><br/>
     <NewReview />
     <Link to='/progress'>See your progress page</Link>
   </div>;
