7168689dccfd4d106cf6ff82a133271a303cdfb5
Connie (Moon Hyung) Choi <connie_choi89@hotmail.com>
2018-01-04 15:53:19 -0800

    changed rating number to stars on completion page

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,9 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App/';
 import registerServiceWorker from './registerServiceWorker';
+import Rater from 'react-rater'
+import 'react-rater/lib/react-rater.css'
+
 
 ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
 registerServiceWorker();
