6d3530db6b6547e00ea4b78a69fe7452d7996bbf
krsnachandra <chandra@Krsnas-MacBook-Air.local>
2018-01-04 13:20:16 -0800

    changing progress layout

--- client/src/App/Progress.jsx
+++ client/src/App/Progress.jsx
@@ -2,18 +2,46 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 function Progress() {
-  return <div>
+  return <div className="container">
     <br/>
     <h1 align="center">My course progress</h1>
     <br/>
       <div align="center">
-        <img src="/htmlcss.png" alt="htmlcss" height="400" width="240"/>
+        <div className="row">
+          <div className="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+            <div className="card">
+              <div className="card-img-container">
+                <img src="https://s3.amazonaws.com/thinkific/courses/course_card_image_000/071/5581490308310.original.jpg?1490308310" alt="" className="card-img-top" />
+              </div>
+              <div className="card-body">
+
+                <div className="row">
+                  <div className="col-md-12">
+                    <div className="card-instructor card-title">
+                      Maggie Moss
+                    </div>
+                    <h4 className="card-title">
+                      HTML &amp; CSS Essentials
+                    </h4>
+                  </div>
+                </div>        
+              </div>
+
+              <div className="row">
+                <div className="col-md-12">
+                  <a href="/courses/html-and-css" className="btn btn-primary btn-block">Start Course</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        {/* <img src="/htmlcss.png" alt="htmlcss" height="400" width="240"/>
         <Link to='/course'>
           <img src="/js1progress.png" alt="js1" height="400" width="240"/>
         </Link>
         <img src="/js2.png" alt="js2" height="400" width="240"/>
-        <img src="/ios.png" alt="ios" height="400" width="240"/>
+        <img src="/ios.png" alt="ios" height="400" width="240"/> */}
       </div>
+    </div>
   </div>;
 }
 
