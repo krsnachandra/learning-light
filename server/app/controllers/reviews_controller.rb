619c6d8f275ae1ac334142460a264a29a5c60338
Connie (Moon Hyung) Choi <connie_choi89@hotmail.com>
2018-01-04 18:34:30 -0800

    working on reviews

---
+++ server/app/controllers/reviews_controller.rb
@@ -0,0 +1,16 @@
+class ReviewsController < ApplicationController
+
+  def create
+    review = Review.new(review_params)
+    if !review.save
+      render json:review.errors.messages, status: 400
+    end
+  end
+
+
+  private 
+
+  def review_params
+    params.require(:review).permit(:rating, :review, :show_flag, :course_id, :user_id)
+  end
+end
