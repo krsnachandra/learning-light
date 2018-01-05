35c0546fa2c483c5f173bc7e69837adedd6b2fb9
krsnachandra <chandra@Krsnas-MacBook-Air.local>
2018-01-04 16:17:51 -0800

    added react-rater

--- client/src/index.js
+++ client/src/index.js
@@ -5,8 +5,8 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App/';
 import registerServiceWorker from './registerServiceWorker';
-import Rater from 'react-rater'
-import 'react-rater/lib/react-rater.css'
+import Rater from 'react-rater';
+import 'react-rater/lib/react-rater.css';
 
 
 ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
