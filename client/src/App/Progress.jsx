26c77f9327d1662990e3e6dd4c7df4985d32734e
V. Anastassiou <v.anastassiou@gmail.com>
2018-01-04 18:21:50 -0800

    Fixed whitespace, added TODOs

--- client/src/App/Progress.jsx
+++ client/src/App/Progress.jsx
@@ -13,7 +13,10 @@ class Progress extends Component {
   componentDidMount(){
     getAllCourses()
     .then((courses) => {
-      this.setState({loading: undefined, courses: courses});
+      this.setState({
+        loading: undefined,
+        courses: courses
+      });
     })
   }
 
@@ -21,67 +24,73 @@ class Progress extends Component {
     if (this.state.loading) {
       return (<Loading />);
     }
-  return <div className="container">
-    <br/>
-    <h1 align="center">My course progress</h1>
-    <br/>
-    <div align="center">
 
-    {this.state.courses.map(function(course){
-      return <div key={ course.id }>
+    return (
+      <div className="container">
+
+        <div id="banner" className="justify-content-center">
+          <h1>My course progress</h1>
+        </div>
+
         <div className="row">
           <div className="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-            <div className="card">
-              <div className="card-img-container">
-                <img src="https://s3.amazonaws.com/thinkific/courses/course_card_image_000/071/5581490308310.original.jpg?1490308310" alt="" className="card-img-top" />
-              </div>
-              <div className="card-body">
 
-                <div className="row">
-                  <div className="col-md-12">
-                    <div className="card-instructor card-title">
-                      Maggie Moss
-                    </div>
-                    <h4 className="card-title">
-                      HTML &amp; CSS Essentials
-                    </h4>
-                  </div>
+            {/* Generate card for each course */}
+
+            {this.state.courses.map(function(course) {
+              return (
+                <div key={ course.id } className="card">
+  	              <div className="card-img-container">
+  		              <img src="https://s3.amazonaws.com/thinkific/courses/course_card_image_000/197/6151508709032.original.jpg?1508709032" alt="" className="card-img-top" />
+
+                    {/* TODO: update course table in DB with 'image' column containing pic
+                      filename and replace above line with:
+                        <img src=`/{course.course_pic}` alt="" className="card-img-top" />
+                     */}
+                     
+  	              </div>
+    	            <div className="card-body">
+    		            <div className="row">
+    			            <div className="col-md-12">
+    				            <div className=" card-instructor card-title">
+    					            {course.instructor.name}
+    				            </div>
+    				              <h4 className="card-title">
+    					              {course.name}
+    				              </h4>
+    				              <p className="card-text">
+    					              Insert card blurb code here.
+
+                            {/* TODO: update course table in DB with 'blurb' column and
+                              replace above line with:
+                                <p>{course.blurb}</p>
+                             */}
+
+    				              </p>
+                      </div>
+    			          </div>
+    		          </div>
+    	            <div className="row">
+  		              <div className="col-md-12">
+  			              <Link to="/js-essentials-2"><button className="btn btn-primary btn-block">Resume course</button></Link>
+
+                      {/* TODO: update course table in DB with 'path' column and
+                        replace above line with:
+                        <Link to=`/{course.path}`><button className="btn btn-primary btn-block">Resume course</button></Link>
+                        */}
+
+  		              </div>
+  	              </div>
                 </div>
-              </div>
+              )
+            })}
 
-              <div className="row">
-                <div className="col-md-12">
-                  <div className="card-instructor card-title">
-                    {course.instructor.name}
-                  </div>
-                  <h4 className="card-title">
-                    {course.name}
-                  </h4>
-                </div>
-              </div>
-            </div>
+            {/* End card generator */}
 
-            <div className="row">
-              <div className="col-md-12">
-                <Link to={`/${course.coursename}/intro`} className="btn btn-primary btn-block">Start Course</Link>
-              </div>
-            </div>
-          </div>
         </div>
-      </div>;
-    })}
-
-
-        {/* <img src="/htmlcss.png" alt="htmlcss" height="400" width="240"/>
-        <Link to='/course'>
-          <img src="/js1progress.png" alt="js1" height="400" width="240"/>
-        </Link>
-        <img src="/js2.png" alt="js2" height="400" width="240"/>
-        <img src="/ios.png" alt="ios" height="400" width="240"/> */}
-
+      </div>
     </div>
-  </div>;
+    )
   }
 }
-
 export default Progress;
