617cf53917ba743d7c3a28a6a421521f4dcaa016
V. Anastassiou <v.anastassiou@gmail.com>
2018-01-04 16:59:36 -0800

    Fixed whitespace

--- client/src/App/CourseSidebar.jsx
+++ client/src/App/CourseSidebar.jsx
@@ -17,24 +17,27 @@ class CourseSidebar extends Component {
       this.setState({loading: undefined, ...course});
     })
   }
-  
+
   render() {
-    console.log(this.props);
     if (this.state.loading) {
       return (<Loading />);
     }
     return (
       <div>
-        {this.state.chapters.map(function(chapter, index){
-            return <div key={ index }>
+        {this.state.chapters.map(function(chapter, index) {
+          return (
+            <div key={ index }>
               <h5>{chapter.name}</h5>
-              {chapter.sections.map(function(section, index){
-              return <div key={ index }>
-                <li><Link to={`/js-essentials-2/${section.sectionname}`}>{section.name}</Link></li>
-              </div>;
-            })}
-            </div>;
-          })}
+              {chapter.sections.map(function(section, index) {
+                return (
+                  <div key={ index }>
+                    <li><Link to={`/js-essentials-2/${section.sectionname}`}>{section.name}</Link></li>
+                  </div>
+                )
+              })}
+            </div>
+          );
+        })}
         <span className='text-success'>{'\u2714'}</span>
       </div>
     );
