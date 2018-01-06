2c98f6cbfa16a893d629c5fc91c16891e6b36467
V. Anastassiou <v.anastassiou@gmail.com>
2018-01-05 22:19:50 -0800

    Can now dynamically fetch card image paths

--- client/src/App/Home.jsx
+++ client/src/App/Home.jsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 import Rater from 'react-rater'
+import {getAllCourses, Loading} from '../course-service';
 import 'react-rater/lib/react-rater.css'
 
-export default function Home() {
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true
+    };
+  }
+
+  componentDidMount(){
+    getAllCourses()
+    .then((courses) => {
+      this.setState({
+        loading: undefined,
+        courses: courses
+      });
+    })
+  }
+
+  render() {
+    if (this.state.loading) {
+      return (<Loading />);
+    }
+
   return (
     <div className="container">
 
@@ -13,7 +36,42 @@ export default function Home() {
 
       <div className="row">
         <div className="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-          <div className="card">
+
+{/* Generate card for each course */}
+
+{this.state.courses.map(function(course) {
+              return (
+                <div key={ course.id } className="card">
+  	              <div className="card-img-container">
+  		              <img src={`/card-${course.coursename}.png`} alt="" className="card-img-top" />
+  	              </div>
+    	            <div className="card-body">
+    		            <div className="row">
+    			            <div className="col-md-12">
+    				            <div className="card-instructor">
+    					            {course.instructor.name}
+    				            </div>
+    				              <h4 className="card-title">
+    					              {course.name}
+    				              </h4>
+    				              <p className="card-text">
+                            {course.blurb}
+    				              </p>
+                      </div>
+    			          </div>
+    		          </div>
+    	            <div className="row">
+  		              <div className="col-md-12">
+  			              <Link to={`/${course.coursename}`}><button className="btn btn-primary btn-block">View Course</button></Link>
+  		              </div>
+  	              </div>
+                </div>
+              )
+            })}
+
+            {/* End card generator */}
+
+          {/* <div className="card">
             <div className="card-img-container">
               <img src="https://s3.amazonaws.com/thinkific/courses/course_card_image_000/071/5581490308310.original.jpg?1490308310" alt="" className="card-img-top" />
             </div>
@@ -127,9 +185,14 @@ export default function Home() {
               <Link to="/js-essentials-2"><button className="btn btn-primary btn-block">View free course</button></Link>
             </div>
           </div>
-        </div>
-      </div>    
+        </div> */}
+
+
+
+      </div>
     </div>
   </div>
-  );
+  )};
 }
+
+export default Home;
