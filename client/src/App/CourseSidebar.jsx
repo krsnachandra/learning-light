bedf70bb2fd8276ea922ea681e8fbe5b646a261b
V. Anastassiou <v.anastassiou@gmail.com>
2018-01-05 23:28:26 -0800

    Testing object passing with the power of console logs

--- client/src/App/CourseSidebar.jsx
+++ client/src/App/CourseSidebar.jsx
@@ -12,21 +12,25 @@ class CourseSidebar extends Component {
   }
 
   componentDidMount(){
-    getCourse("js-essentials-2")
+    console.log("THIS PARAMS", this.props.params);
+    console.log("THIS PROPS", this.props);
+    getCourse(this.props.coursename)
     .then((course) => {
+      console.log("COURSE IS", course);
       this.setState({loading: undefined, ...course});
     })
   }
 
   render() {
+
     if (this.state.loading) {
       return (<Loading />);
     }
+
     return (
       <div className="sidebar-wrapper">
         <nav id="sidebar">
           <ul>
-
             {/* Begin creating headings from chapter names */}
             {this.state.chapters.map(function(chapter) {
               return (
@@ -52,7 +56,7 @@ class CourseSidebar extends Component {
               );
             })}
 
-            {/* End creating headings from chapter names */}
+            {/* End headings creator */}
           </ul>
         </nav>
       <span className='text-success'>{'\u2714'}</span>
