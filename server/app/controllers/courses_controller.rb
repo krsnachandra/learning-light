8cab5fb5810c56d1e9e82da76aeaa79b2a37ef0e
krsnachandra <chandra@Krsnas-MacBook-Air.local>
2018-01-07 14:39:09 -0800

    added reviews to courses endpoint for Homepage rendering of avg review

--- server/app/controllers/courses_controller.rb
+++ server/app/controllers/courses_controller.rb
@@ -2,7 +2,7 @@ class CoursesController < ApplicationController
 
     def index
         @courses = Course.all.order(id: :asc)
-        render json: @courses.to_json(include: :instructor)
+        render json: @courses.to_json(include: [:instructor, :reviews])
     end
 
     def show
