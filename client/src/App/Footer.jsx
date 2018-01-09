81965246460e941f06271295c61d11feb4717163
V. Anastassiou <v.anastassiou@gmail.com>
2018-01-09 13:31:17 -0800

    Fixed footer to bottom of page

--- client/src/App/Footer.jsx
+++ client/src/App/Footer.jsx
@@ -2,24 +2,26 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <div className="navbar navbar-expand-lg navbar-dark bg-dark">
-    <footer>
-      <div className="text-center">
-        <div className="mx-auto">
+    <div className="fixed-bottom navbar navbar-expand-lg navbar-dark bg-dark justify-content-center">
+      <div className="column">
+        <div className="row footer-social-media justify-content-center">
           <ul className="nav">
             <li className="nav-item"><a href="https://twitter.com/lighthouse_labs" target="0"><i className="fa fa-twitter fa-2x"></i></a></li>
             <li className="nav-item"><a href="https://www.facebook.com/lighthouselabsvancouver" target="0"><i className="fa fa-facebook fa-2x"></i></a></li>
             <li className="nav-item"><a href="https://plus.google.com/+LighthouselabsCan/posts" target="0"><i className="fa fa-google-plus fa-2x"></i></a></li>
             <li className="nav-item"><a href="https://www.linkedin.com/company/lighthouse-labs" target="0"><i className="fa fa-linkedin fa-2x"></i></a></li>
           </ul>
+        </div>
+        <div className="row footer-links justify-content-center">
           <ul className="nav">
             <li className="nav-item"><a href="/">Home</a></li>
             <li className="nav-item"><a href="https://www.lighthouselabs.ca/contact" target="0">Contact us</a></li>
           </ul>
+        </div>
+        <div className="row footer-blurb justify-content-center">
           <small>Lighthouse Labs © 2018</small>
         </div>
       </div>
-    </footer>
-  </div>
+    </div>
   );
 }
