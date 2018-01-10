d6e6c87b760c4dee0d4a8f4ab6582e71413ec6db
V. Anastassiou <v.anastassiou@gmail.com>
2018-01-10 15:48:15 -0800

    Tolerable footer styling

--- client/src/App/Footer.jsx
+++ client/src/App/Footer.jsx
@@ -3,15 +3,14 @@ import React from 'react';
 export default function Footer() {
   return (
     <div className="navbar footer bg-dark">
-      <div class="row">
-    <div class="col-sm-6">
-      <div className="row footer-links">
+    <div class="col-sm-6 col-lg-3">
+      <div className="row d-flex justify-content-start">
         <ul className="nav">
           <li className="nav-item"><a href="/">Home</a></li>
           <li className="nav-item"><a href="https://www.lighthouselabs.ca/contact" target="0">Contact us</a></li>
         </ul>
       </div>
-      <div className="row footer-social-media">
+      <div className="row d-flex justify-content-start">
         <ul className="nav">
           <li className="nav-item"><a href="https://twitter.com/lighthouse_labs" target="0"><i className="fa fa-twitter fa-2x"></i></a></li>
           <li className="nav-item"><a href="https://www.facebook.com/lighthouselabsvancouver" target="0"><i className="fa fa-facebook fa-2x"></i></a></li>
@@ -19,32 +18,24 @@ export default function Footer() {
           <li className="nav-item"><a href="https://www.linkedin.com/company/lighthouse-labs" target="0"><i className="fa fa-linkedin fa-2x"></i></a></li>
         </ul>
       </div>
-      <div class="row">
-        <div className="row footer-blurb">
-          <small>Lighthouse Labs © 2018</small>
-        </div>
+      <div className="row footer-blurb d-flex justify-content-start">
+          Lighthouse Labs © 2018
       </div>
     </div>
-    <div class="col-sm-6">
-      <div className="row footer-links footer-blurb">
+    <div class="col-sm-6 col-lg-3">
+      <div className="row footer-links footer-blurb d-flex justify-content-end">
         <ul className="nav">
-          <li className="nav-item">Made by</li>
+          <li className="nav-item">Made with <i class="fa fa-heart" aria-hidden="true"></i> by</li>
         </ul>
       </div>
-      <div className="row footer-social-media">
-        <ul className="nav">
-          <li><a href="https://github.com/krsnachandra"><img src="/portrait-cc.jpg" className="rounded-circle icon-portrait" /></a></li>
-          <li><a href="https://github.com/krsnachandra"><img src="/portrait-cm.jpg" className="rounded-circle icon-portrait" /></a></li>
+      <div className="row d-flex justify-content-end">
+        <ul className="nav footer-icons">
           <li><a href="https://github.com/vanastassiou"><img src="/portrait-va.jpg" className="rounded-circle icon-portrait" /></a></li>
+          <li><a href="https://github.com/conniechoi89"><img src="/portrait-cc.jpg" className="rounded-circle icon-portrait" /></a></li>
+          <li><a href="https://github.com/krsnachandra"><img src="/portrait-cm.jpg" className="rounded-circle icon-portrait" /></a></li>
         </ul>
       </div>
-      <div class="row">
-        <div className="row footer-blurb">
-          with <i class="fa fa-heart" aria-hidden="true"></i>
-        </div>
-      </div>
     </div>
-  </div>
 </div>
 );
 }
